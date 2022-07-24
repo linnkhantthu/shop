d8e919373cde35bb8a4e8fa91bfbdb85eac08877
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2022-07-24 12:24:59 +0630

    added purchase_price, extra_costs, percentage columns to Product Model

--- shop/inventory/models.py
+++ shop/inventory/models.py
@@ -14,6 +14,9 @@ class Products(db.Model):
     image = db.Column(db.String(20), nullable=False, default='favicon.ico')
     unit = db.Column(db.String(10), nullable=False)
     price = db.Column(db.Float, nullable=False)
+    purchase_price = db.Column(db.Float)
+    extra_costs = db.Column(db.Float)
+    percentage = db.Column(db.Float)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     p_type = db.Column(db.String(20), nullable=False)
 
