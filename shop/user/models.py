8f34c821b756669ded8d8e3c7428aa4ac4590220
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2022-07-24 18:56:59 +0630

    changed password string size

--- shop/user/models.py
+++ shop/user/models.py
@@ -2,21 +2,25 @@ from flask import current_app
 from shop import db, login_manager
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     account_type = db.Column(db.String(10), default='normal')
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(120), nullable=False)
     products = db.relationship('Products', backref='user', lazy=True)
     unitchoices = db.relationship('UnitChoices', backref='user', lazy=True)
-    producttypechoices = db.relationship('ProductTypeChoices', backref='user', lazy=True)
-    
+    producttypechoices = db.relationship(
+        'ProductTypeChoices', backref='user', lazy=True)
+
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
